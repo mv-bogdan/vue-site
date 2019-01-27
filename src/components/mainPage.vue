64cafb6e22e7c541122a84293d8e425177815166
mv-bogdan <mv-bogdan@yandex.ru>
2019-01-27 23:42:05 +0300

    adding content (1)

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -12,18 +12,36 @@
                     width="75%"
                     maxwidth="90%"
             >
-                <v-jumbotron>
-                    <v-container fill-height>
+                    <v-container>
                         <v-layout>
                             <v-flex>
-                                <h8 class="display-1">Главная страница</h8>
-
-                                <v-divider class="my-2"></v-divider>
-                                <span class="subheading">*Информация обо мне.*</span>
+                                <h8 class="display-1">Обо мне</h8>
+                                <v-divider class="my-1"></v-divider>
                             </v-flex>
-                        </v-layout>
+                         </v-layout>
                     </v-container>
-                </v-jumbotron>
+                    <v-container wrap row>
+                        <v-flex xs12 sm3>
+                            <v-card
+                                :elevation = "12"
+                            >
+                                <v-img
+                                   :src="`https://pp.userapi.com/c845523/v845523597/8e58f/6BonQGUGGDQ.jpg`"
+                                >
+                                </v-img>
+                            </v-card>
+                        </v-flex>
+                        <!--<v-flex xs12 sm9>
+                            <v-img
+                                    :src="`http://www.vesvks.ru/public/article/images/ed8e13c307e611a5e05f82eb77ca30dd7d27eb2d.jpg`"
+                            >
+                            </v-img>
+                        </v-flex>-->
+                        <v-flex xs12 sm9>
+                            <span class="subheading">Меня зовут Богдан, я - студент {{dateYear - 2018}}-го курса МГТУ им. Баумана. Учусь на кафедре ФН-12 "Математическое моделирование" </span>
+                        </v-flex>
+
+                    </v-container>
             </v-sheet>
       </v-layout>
     </v-slide-y-transition>
@@ -35,7 +53,13 @@
         name: "mainPage",
         data: () => ({
             elevations: [18]
-        })
+        }),
+        computed: {
+            dateYear: function () {
+                let date = new Date();
+                return date.getFullYear()
+            }
+        }
     }
 </script>
 
